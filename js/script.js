12f42b00cabb5a363154685c5a1daead3e5eb524
bmit-elc <chaimaa.eljarite@espas.ch>
2023-10-17 11:37:20 +0200

    new background. Responsivein +sizes. new animation

--- js/script.js
+++ js/script.js
@@ -2,3 +2,27 @@ const d = new Date();
 let year = d.getFullYear();
 document.getElementById("year").innerHTML = year;
 
+document.addEventListener("DOMContentLoaded", function () {
+  const hero = document.querySelector(".hero");
+  const navBar = document.querySelector(".nav-bar");
+  const image = document.querySelector(".image");
+  const heroTitle = document.querySelector(".hero-title");
+
+  hero.style.display = "block";
+
+  setTimeout(() => {
+    hero.style.width = "max-content"; // Expand the hero to its full content
+    hero.style.opacity = 1; // Make the hero visible (fade in)
+    navBar.style.transform = "translateY(0%)"; // Move the navbar back to its original position
+    navBar.style.opacity = 1; // Make the navbar visible (fade in)
+    image.style.transform = "translateY(0%)"; // Move the image back to its original position
+    image.style.opacity = 1; // Make the image visible (fade in)
+  }, 1500); // After 1.5 seconds
+
+  setTimeout(() => {
+    heroTitle.style.width = "max-content"; // Expand the hero-title to its full content
+    heroTitle.style.opacity = 1; // Make the hero-title visible (fade in)
+    heroTitle.style.animation = "typewriter 3s steps(24) 0.1s 1 normal both, blinkTextCursor 800ms steps(24) infinite normal";
+    // Start the typewriter animation after a delay
+  }, 3000); // After 1.5 seconds
+});
